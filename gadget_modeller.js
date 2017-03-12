316ded5ef39b3d1b38cd3e03582cf3d14e4401cc
Sven Franck <svenfranck@yahoo.com>
2017-03-12 22:13:38 +0100

    first punt at building grammar/voca from user input

--- gadget_modeller.js
+++ gadget_modeller.js
@@ -6,6 +6,9 @@
   /////////////////////////////
   // templates
   /////////////////////////////
+  var LINE_BREAKS = /(.*?[\r\n])/g;
+  var ALPHA_NUMERIC = /[^A-Z0-9.]/gi;
+
   var TEXT = {"format": "text"};
   var MODEL = "modeller";
   
@@ -17,7 +20,7 @@
   S : NS_B SENT NS_E\n\
   SENT: CALL_V NAME_N\n\
   SENT: DIAL_V DIGIT";
-
+                              
   var VOCA_PLACEHOLDER = "Please enter vocabulary, format:\n\
   % CALL_V\n\
   PHONE     f ow n\n\
@@ -54,7 +57,7 @@
     } else {
       text = "";
     }
-    console.log("got text, ", text)
+
     return my_gadget.jio_getAttachment(MODEL, my_name, TEXT)
       .push(undefined, function (my_error) {
         if (isNotFound(my_error)) {
@@ -79,7 +82,14 @@
         element = gadget.element;
 
       gadget.property_dict = {
-        "is_active_storage": false,
+        "modeller_count": 0,
+        "voca_init": "",
+        "grammar_init": "% NS_B\n<s>       sil\n\n% NS_E\n<s>        sil\n",
+        "voca_text": null,
+        "grammar_text": null,
+        "status": element.querySelector(".kw-modeller-status input"),
+        "voca": element.querySelector(".kw-modeller-voca input"),
+        "grammar": element.querySelector(".kw-modeller-grammar input"),
         "modeller_input": element.querySelector(".kw-input textarea"),
         "modeller_output": element.querySelector(".kw-output textarea")
       };
@@ -88,12 +98,14 @@
     /////////////////////////////
     // acquired methods
     /////////////////////////////
+    .declareAcquiredMethod('jio_create', 'jio_create')
     .declareAcquiredMethod('jio_allDocs', 'jio_allDocs')
     .declareAcquiredMethod('jio_createJio', 'jio_createJio')
     .declareAcquiredMethod('jio_putAttachment', 'jio_putAttachment')
     .declareAcquiredMethod('jio_getAttachment', 'jio_getAttachment')
     .declareAcquiredMethod('jio_put', 'jio_put')
     .declareAcquiredMethod('jio_get', 'jio_get')
+    .declareAcquiredMethod('validateAgainstDict', 'validateAgainstDict')
 
     /////////////////////////////
     // published methods
@@ -106,12 +118,15 @@
       var gadget = this,
         props = gadget.property_dict;
 
-      //props.voca.setAttribute("placeholder", VOCA_PLACEHOLDER);
-      //props.grammar.setAttribute("placeholder", GRAMMAR_PLACEHOLDER);
       props.modeller_input.setAttribute("placeholder", INPUT_PLACEHOLDER);
+      props.status.className = "kw-modeller-active-tab";
 
       return new RSVP.Queue()
-        .push(function () {
+        //.push(function () {
+        //  return gadget.jio_create({"type": "memory"});
+        //})
+        .push(function (my_memory_storage) {
+          //props.storage = my_memory_storage;
           return gadget.jio_get("modeller");
         })
         .push(undefined, function (my_error) {
@@ -128,6 +143,52 @@
         //});
     })
 
+    .declareMethod("convertInput", function () {
+      var gadget = this,
+        props = gadget.property_dict,
+        input_value = props.modeller_input.value,
+        word_list;
+
+      if (input_value === "") {
+        return;
+      }
+
+      return new RSVP.Queue()
+        .push(function () {
+          var line_list = input_value.toUpperCase()
+              .split(LINE_BREAKS).filter(Boolean),
+            utter_tag;
+          
+          word_list = line_list.map(function (line) {
+            return line.split(" ").reduce(function (prev, next) {
+              return prev += "W_" + next.replace(ALPHA_NUMERIC, '') + " ";
+            }, "");
+          }).join("").trim();
+
+          props.modeller_count += 1;
+          utter_tag = "UTTER_" + props.modeller_count;
+          props.voca_text = (props.voca_text || props.voca_init) + "S : NS_B " +
+            utter_tag + " NS_E\n" + utter_tag + ":" + word_list + "\n";
+
+          return gadget.validateAgainstDict(word_list.replace(/W_/g, ""));
+        })
+        .push(function (my_validation_dict) {
+          if (my_validation_dict.error_list.length > 0) {
+            // output error report in status
+          }
+
+          props.grammar_text = (props.grammar_text || props.grammar_init) + 
+            word_list.split(" ").reduce(function (prev, next) {
+              var clean = next.replace("W_", "");
+              return prev += "\n% " + next + "\n" + clean + "       " + 
+                my_validation_dict.match_dict[clean] + "\n";
+            }, "");
+          console.log(props);
+          props.modeller_output.value = props.grammar_text;
+        })
+        
+    })
+
     /////////////////////////////
     // declared job
     /////////////////////////////
@@ -142,12 +203,20 @@
     .onEvent("submit", function (my_event) {
       var gadget = this;
       switch (my_event.target.name) {
-        case "kw-modeller-input":
-          console.log("store input")
-          return storeBlob(gadget, my_event, "input");
-        case "kw-modeller-output":
-          console.log("store output")
-          return storeBlob(gadget, my_event, "output");
+        case "kw-modeller-storage-load": break;
+        case "kw-modeller-input-save": break;
+        case "kw-modeller-input-open": break;
+        case "kw-modeller-input-delete": break;
+        case "kw-modeller-input-test":
+          return gadget.convertInput();
+        case "kw-modeller-output-tab-grammar": break;
+        case "kw-modeller-output-tab-status": break;
+        case "kw-modeller-output-tab-voca": break;
+        case "kw-modeller-output-save": break;
+        case "kw-modeller-output-open": break;
+        case "kw-modeller-output-delete": break;
+        case "kw-modeller-output-build": break;
+        case "kw-modeller-output-download": break;
         default:
           return false;
       }
