068e82a470c6d8cbfcab3b804f0c406f686efb52
Sven Franck <svenfranck@yahoo.com>
2017-03-24 09:17:36 +0100

    add custom lexer

--- xxx_grammar.js
+++ xxx_grammar.js
@@ -454,29 +454,59 @@ remark
   | NL
   ;
 
-/* #include "lex.yy.c" => see custom lexer */
-
 %%
+/* #include "lex.yy.c" => see custom lexer */
+%{
+  // myscanner.js
+  function AlphabetScanner() {
+    console.log("WHAT THE FUCK")
+      var text = "";
+      this.yytext = "";
+      this.yyloc = {
+          first_column: 0,
+          first_line: 1,
+          last_line: 1,
+          last_column: 0
+      };
+      this.yylloc = this.yyloc;
+      this.setInput = function(text_) {
+        console.log("SETTING INPUT")
+          text = text_;
+      };
+      this.lex = function() {
+          // Return the EOF token when we run out of text.
+          if (text === "") {
+              return "EOF";
+          }
+  
+          // Consume a single character and increment our column numbers.
+          var c = text.charAt(0);
+          text = text.substring(1);
+          this.yytext = c;
+          this.yyloc.first_column++;
+          this.yyloc.last_column++;
+  
+          if (c === "\n") {
+              // Increment our line number when we hit newlines.
+              this.yyloc.first_line++;
+              this.yyloc.last_line++;
+              // Try to keep lexing because we aren't interested
+              // in newlines.
+              return this.lex();
+          } else if (/[a-z]/.test(c)) {
+              return "LOWER_CASE";
+          } else if (/[A-Z]/.test(c)) {
+              return "UPPER_CASE";
+          } else if (/\s/.test(c)) {
+              // Try to keep lexing because we aren't interested
+              // in whitespace.
+              return this.lex();
+          } else {
+              return "INVALID";
+          }
+      };
+  }
+  parser.lexer = new AlphabetScanner();
+}%
 
-
-
-/*
-void setGram( void )
-{
-    char *name;
-
-    yyparse();
-    if( !SW_Quiet ){
-	fprintf( stderr, "\rNow modifying grammar to minimize states[%d]\n", grammar_modification_number - 1 );
-	NoNewLine = 0;
-    }
-    if( StartSymbol == NULL ) StartSymbol = ClassList;
-    fprintf( FPheader, "// Start Symbol: %s \n", StartSymbol->name );
-    fclose( FPheader );
-    if( (name = chkNoInstantClass()) != NULL ){
-	errMes( "Prototype-declared Class \"%s\" has no instant definitions", name );
-    }
-    if( error_count ) errMes( "%d fatal errors exist", error_count );
-}
-*/
 
